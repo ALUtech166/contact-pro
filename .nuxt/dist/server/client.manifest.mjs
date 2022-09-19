f4c667ae63ee8361e41a33f0471dad62745c3cba
ALUtech166 <s.tassiga@alustudent.com>
2022-09-19 17:22:20 +0000

    update

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///C:/Users/MEG/Desktop/Nuxtjs/contact-pro/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///C:/Users/MEG/Desktop/contact-pro/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
